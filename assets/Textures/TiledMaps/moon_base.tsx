22a369f2b484935aa1aac5514657731c36500e88
Acquati <leandroacquati@gmail.com>
2022-09-27 07:22:28 -0300

    add tiled map

---
+++ assets/Textures/TiledMaps/moon_base.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="moon_base" tilewidth="16" tileheight="16" tilecount="210" columns="14">
+ <image source="moon_base.png" width="224" height="240"/>
+ <tile id="121" probability="0.007"/>
+ <tile id="124" probability="0.007"/>
+ <tile id="125" probability="0.007"/>
+ <tile id="135" probability="0.007"/>
+ <tile id="138" probability="0.007"/>
+ <tile id="139" probability="0.007"/>
+ <tile id="148" probability="0.007"/>
+ <tile id="149" probability="0.007"/>
+</tileset>
