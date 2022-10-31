aaad1dea4c530b93072f8d0986df41c7eee112ea
Acquati <leandroacquati@gmail.com>
2022-10-31 03:43:58 -0300

    finish HeadsTails scene

--- assets/Scripts/HeadsTails/HeadsTailsSceneManager.ts
+++ assets/Scripts/HeadsTails/HeadsTailsSceneManager.ts
@@ -1,5 +1,5 @@
 import exactMath from 'exact-math'
-import { _decorator, Component, Node, randomRangeInt, Animation } from 'cc'
+import { _decorator, Component, Node, randomRangeInt, Animation, Label } from 'cc'
 
 const { ccclass, property } = _decorator
 
@@ -8,78 +8,26 @@ export class HeadsTailsSceneManager extends Component {
   @property({ type: [Node] })
   private coins: Node[] = []
 
-  private _wallet: number = 0
-  private _bet: number = 2
-  private _victories: number = 0
-  private _defeats: number = 0
+  @property({ type: Label })
+  private hitLabel: Label | null = null
+
+  @property({ type: Label })
+  private multiplierLabel: Label | null = null
+
+  @property({ type: Label })
+  private resultLabel: Label | null = null
+
+  @property({ type: Label })
+  private walletLabel: Label | null = null
+
+  private _wallet: number = 100
+  private _hit: number = 2
+  private _bet: number = 10
   private _coinFlips: number = 0
   private _flips: boolean[] = []
+  private _animationDelay: number = 0
 
   onLoad() {
-    for (let j = 1; j <= 100000; j++) {
-      this._wallet -= 10
-
-      let heads: number = 0
-      for (let i = 1; i <= 5; i++) {
-        if (randomRangeInt(0, 2) === 0) {
-          heads += 1
-        }
-      }
-
-      if (this._bet === heads) {
-        this._victories += 1
-
-        switch (this._bet) {
-          case 0:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 1) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-          case 1:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 5) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-          case 2:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 10) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-          case 3:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 10) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-          case 4:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 5) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-          case 5:
-            this._wallet = Number(
-              exactMath.round(exactMath.add(this._wallet, 10 * (32 / 1) * 0.99), -2, {
-                returnString: true,
-              })
-            )
-            break
-        }
-      } else {
-        this._defeats += 1
-      }
-    }
-
-    console.log(this._wallet, this._victories, this._defeats)
-
     for (let i = 0; i < 5; i++) {
       if (randomRangeInt(0, 2) === 0) {
         this._flips[i] = true
@@ -87,14 +35,78 @@ export class HeadsTailsSceneManager extends Component {
         this._flips[i] = false
       }
     }
-  }
 
-  start() {
-    // this.coins[0].getComponent(Animation).play('coinFlip')
+    this.hitLabel.string = 'To hit: ' + this._hit + ' Heads | Bet: HC$' + this._bet
+
+    let betMultiplier: number = 0
+
+    switch (this._hit) {
+      case 0:
+      case 5:
+        betMultiplier = Number(
+          exactMath.floor((32 / 1) * 0.99, -2, {
+            returnString: true,
+          })
+        )
+        break
+      case 1:
+      case 4:
+        betMultiplier = Number(
+          exactMath.floor((32 / 5) * 0.99, -2, {
+            returnString: true,
+          })
+        )
+        break
+      case 2:
+      case 3:
+        betMultiplier = Number(
+          exactMath.floor((32 / 10) * 0.99, -2, {
+            returnString: true,
+          })
+        )
+        break
+    }
+
+    this.multiplierLabel.string = 'Bet multiplier: ' + betMultiplier + 'X'
+
+    let heads: number = 0
+    this._flips.forEach((value) => {
+      if (value) {
+        heads += 1
+      }
+    })
+
+    if (this._hit === heads) {
+      const prize = Number(
+        exactMath.floor(exactMath.mul(this._bet, betMultiplier), -2, {
+          returnString: true,
+        })
+      )
+
+      this._wallet = Number(
+        exactMath.ceil(exactMath.add(this._wallet, prize), -2, {
+          returnString: true,
+        })
+      )
+
+      this.resultLabel.string = heads + ' Heads, you win HC$' + prize
+    } else {
+      this._wallet = Number(
+        exactMath.floor(exactMath.sub(this._wallet, this._bet), -2, {
+          returnString: true,
+        })
+      )
+
+      this.resultLabel.string = heads + ' Heads, you loose HC$' + this._bet
+    }
+
+    this.walletLabel.string = 'Wallet: HC$' + this._wallet
   }
 
   update(deltaTime: number) {
-    if (this._coinFlips <= 9) {
+    if (this._animationDelay < 1) {
+      this._animationDelay += deltaTime
+    } else if (this._coinFlips <= 9) {
       if (!this.coins[0].getComponent(Animation).getState('coinFlip').isPlaying && this._coinFlips === 0) {
         this.coins[0].getComponent(Animation).play('coinFlip')
         this._coinFlips += 1
@@ -145,6 +157,8 @@ export class HeadsTailsSceneManager extends Component {
           this.coins[4].getComponent(Animation).play('tail')
         }
         this._coinFlips += 1
+        this.resultLabel.node.active = true
+        this.walletLabel.node.active = true
       }
     }
   }
