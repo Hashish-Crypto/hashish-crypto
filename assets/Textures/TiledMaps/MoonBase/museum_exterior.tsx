08f2679436412a3668b3c4a3d42b0eb2b588d2f6
Samir Kazah <49498645+SamirKazah@users.noreply.github.com>
2022-11-18 14:05:08 +0200

    Updated tiles

--- assets/Textures/TiledMaps/MoonBase/museum_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/museum_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="museum" tilewidth="16" tileheight="16" tilecount="380" columns="19">
- <image source="../../../../../../../../../Dropbox/My Designs Dropbox/Hashish/Tiles/Museum/museum_tile_exterior.png" width="304" height="320"/>
+<tileset version="1.9" tiledversion="1.9.2" name="museum" tilewidth="16" tileheight="16" tilecount="399" columns="19">
+ <image source="museum_exterior.png" width="304" height="336"/>
 </tileset>
