342186f4fb269026d963d6f2e43d94f0ce75b9f0
Acquati <leandroacquati@gmail.com>
2022-11-27 00:19:28 -0300

    WIP: add new art content

--- assets/Textures/TiledMaps/MoonBase/museum_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/museum_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="museum" tilewidth="16" tileheight="16" tilecount="399" columns="19">
- <image source="museum_exterior.png" width="304" height="336"/>
+<tileset version="1.9" tiledversion="1.9.2" name="museum_exterior" tilewidth="16" tileheight="16" tilecount="414" columns="18">
+ <image source="museum_exterior.png" width="288" height="368"/>
 </tileset>
